ef1f46e1dfc5581eb22115fa3a53088d03c7512b
mihailytch <140865528+mihailytch@users.noreply.github.com>
2023-07-29 09:12:58 +0700

    Форма с картой и возможностью добавлять метки

---
+++ DroneRouteMap/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DroneRouteMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(12, 12);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(386, 360);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            this.gMapControl1.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.gMapControl1_OnMarkerClick);
+            this.gMapControl1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(404, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 384);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DroneRouteMap/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET.WindowsForms;
+
+namespace DroneRouteMap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        GMap.NET.WindowsForms.GMapOverlay markersOverlay;
+        Route route;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Настройки для компонента GMap.
+            gMapControl1.Bearing = 0;
+
+            //CanDragMap - Если параметр установлен в True,
+            //пользователь может перетаскивать карту 
+            ///с помощью правой кнопки мыши. 
+            gMapControl1.CanDragMap = true;
+
+            //Указываем, что перетаскивание карты осуществляется 
+            //с использованием левой клавишей мыши.
+            //По умолчанию - правая.
+            gMapControl1.DragButton = MouseButtons.Left;
+
+            gMapControl1.GrayScaleMode = true;
+
+            //MarkersEnabled - Если параметр установлен в True,
+            //любые маркеры, заданные вручную будет показаны.
+            //Если нет, они не появятся.
+            gMapControl1.MarkersEnabled = true;
+
+            //Указываем значение максимального приближения.
+            gMapControl1.MaxZoom = 18;
+
+            //Указываем значение минимального приближения.
+            gMapControl1.MinZoom = 2;
+
+            //Устанавливаем центр приближения/удаления
+            //курсор мыши.
+            gMapControl1.MouseWheelZoomType =
+                GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+
+            //Отказываемся от негативного режима.
+            gMapControl1.NegativeMode = false;
+
+            //Разрешаем полигоны.
+            gMapControl1.PolygonsEnabled = true;
+
+            //Разрешаем маршруты
+            gMapControl1.RoutesEnabled = true;
+
+            //Скрываем внешнюю сетку карты
+            //с заголовками.
+            gMapControl1.ShowTileGridLines = false;
+
+            //Указываем, что при загрузке карты будет использоваться 
+            //18ти кратное приближение.
+            gMapControl1.Zoom = 5;
+
+            //Указываем что все края элемента управления
+            //закрепляются у краев содержащего его элемента
+            //управления(главной формы), а их размеры изменяются 
+            //соответствующим образом.
+            //gMapControl1.Dock = DockStyle.Fill;
+
+            //Указываем что будем использовать карты Google.
+            gMapControl1.MapProvider =
+            GMap.NET.MapProviders.GMapProviders.GoogleMap;
+            GMap.NET.GMaps.Instance.Mode =
+                GMap.NET.AccessMode.ServerOnly;
+
+            //Добавляем слой маркеров
+            markersOverlay = new GMap.NET.WindowsForms.GMapOverlay(gMapControl1, "marker");
+            gMapControl1.Overlays.Add(markersOverlay);
+
+            route = new Route();
+
+        }
+
+        private void gMapControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                double lat = gMapControl1.FromLocalToLatLng(e.X, e.Y).Lat;
+                double lng = gMapControl1.FromLocalToLatLng(e.X, e.Y).Lng;
+                
+                GMap.NET.WindowsForms.Markers.GMapMarkerGoogleGreen markerG =
+    new GMap.NET.WindowsForms.Markers.GMapMarkerGoogleGreen(
+    new GMap.NET.PointLatLng(lat, lng));
+                markersOverlay.Markers.Add(markerG);
+
+                route.AddWayPoint(lat, lng);
+            }
+        }
+
+        private void gMapControl1_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+            markersOverlay.Markers.Remove(item);
+        }
+    }
+}
